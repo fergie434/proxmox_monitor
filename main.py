5ab949796592949c608757088b0bbc0ff3f63902
fergie434 <fergie434@gmail.com>
2025-06-01 14:04:15 +0800

    Moved logging to separate module

--- main.py
+++ main.py
@@ -3,8 +3,7 @@
 from proxmoxer import ProxmoxAPI
 from dotenv import load_dotenv
 import os
-import logging
-from logging.handlers import RotatingFileHandler
+import log_helper
 
 load_dotenv()
 proxmox_server = os.getenv('proxmox_server')
@@ -12,22 +11,6 @@ realm = os.getenv('realm')
 username = f"{os.getenv('username')}@{os.getenv('realm')}"
 password = os.getenv('password')
 proxmox = ProxmoxAPI(proxmox_server, user=username, password=password, verify_ssl=False)
-log_filename = 'logs/proxmox_monitor.log'
-log_maxbytes = 4000000
-log_filecount = 5
-
-def setup_logging():
-    logger = logging.getLogger(__name__)
-    logger.setLevel(level=logging.INFO)
-    formatter = logging.Formatter(
-            '%(asctime)s - %(levelname)s - %(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S'
-    )
-    rotating_handler = RotatingFileHandler(filename=log_filename, maxBytes=log_maxbytes, backupCount=log_filecount)
-    rotating_handler.setFormatter(formatter)
-    logger.addHandler(rotating_handler)
-
-    return logger
 
 def main(logger):
     for node in proxmox.nodes.get():
@@ -37,7 +20,7 @@ def main(logger):
         # Check all lxc containers are running
         for lxc in lxc_list:
             lxc_config = node_obj.lxc(lxc['vmid']).config.get()
-            if lxc_config['onboot'] == 1and lxc['status'] == 'stopped':
+            if lxc_config['onboot'] == 1 and lxc['status'] == 'stopped':
                 node_obj.lxc(lxc['vmid']).status.start.post()
                 logger.info(f"Started {lxc['name']}")
 
@@ -52,7 +35,7 @@ def main(logger):
 
 
 if __name__ == '__main__':
-    logger = setup_logging()
+    logger = log_helper.setup_logging(log_filename='proxmox_monitor.log')
     logger.info('Script Starting')
     main(logger)
     logger.info('Script Completed')
